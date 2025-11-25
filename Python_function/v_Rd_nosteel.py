52693ad273b7c58005961f76b3b1d617cf53110d
MaKe-r <kellermarco98@gmail.com>
2025-11-25 12:59:25 +0100

    Modifind the v_Rd calc for the new SIA 262

--- Python_function/v_Rd_nosteel.py
+++ Python_function/v_Rd_nosteel.py
@@ -1,17 +1,13 @@
-def v_Rd_nosteel(tau_cd,d,d_v,f_sd,plastische_Verformung = False,torque_rate = 1,E_s = 205000,D_max = 32):
-    k_g = 48/(16+D_max)
+def v_Rd_nosteel(tau_cd,d,d_v,f_sd,f_ck,torque_rate = 1,E_s = 205000,D_max = 32):
+    k_g = max(1.2,48/(16+(D_max*min(1,(60/f_ck)**2))))
     varepsilon_v = (f_sd/E_s)*torque_rate
-    if plastische_Verformung:
-        varepsilon_v = (f_sd/E_s)*1.5
     k_d = 1/(1+(varepsilon_v*d*k_g))
     v_Rd = k_d * tau_cd * d_v
-    print(f"v_Rd = {round(v_Rd,2)} kN/m'")
+    #print(min(1,(60/f_ck)),k_g, k_d)
+    print(f"v_Rd = {round(v_Rd,2)*2} kN/m'")
 
-v_Rd_nosteel(1.2,590,560,300,True)
-v_Rd_nosteel(1.2,350,300,300,True)
-v_Rd_nosteel(1.2,590,560,300)
-v_Rd_nosteel(1.2,350,300,300)
-v_Rd_nosteel(1.2,590,560,300,False,0.5)
-v_Rd_nosteel(1.2,350,300,300,False,0.5)
-v_Rd_nosteel(1.2,590,560,300,False,0.37)
-v_Rd_nosteel(1.2,350,300,300,False,0.37)
+
+
+v_Rd_nosteel(tau_cd=1.2,d=590,d_v=560,f_sd=300,f_ck=35,torque_rate=0.68)
+v_Rd_nosteel(tau_cd=1.2,d=590,d_v=560,f_sd=300,f_ck=35,torque_rate=1.5)
+v_Rd_nosteel(tau_cd=1.2,d=350,d_v=300,f_sd=300,f_ck=35,torque_rate=1.5)
